e8c7f1188e216858125da7a33ac8aa4eea9771fc
nxe <rosiegbartlett@gmail.com>
2022-12-01 23:56:08 +0000

    oooh. tests

--- Server/src/api.rs
+++ Server/src/api.rs
@@ -3,16 +3,25 @@
 //! This module includes paths for the API used with the Oculus
 
 use rocket::http::{Status, CookieJar};
+use rocket::serde::{Deserialize, json::Json};
 use rocket::Request;
 
 use crate::conf::API_KEY;
 
+#[derive(Deserialize, Debug)]
+pub struct Data {
+	a: String
+}
+
 /// Submit path
 ///
 /// This path is used to submit session data to the server. This required the API key be set via
-/// cookies sent with the request
-#[post("/submit")]
-pub fn submit(cookies: &CookieJar<'_>) -> Status {
+/// cookies sent with the request.
+/// If the given data cannot be serialised into the required struct, this will fail and return a 400
+/// response code (bad request)
+#[post("/submit", data="<data>")]
+pub fn submit(cookies: &CookieJar<'_>, data: Json<Data>) -> Status {
+	println!("{:?}", data);
 	if let Some(c) = cookies.get("key") {
 		if c.value() != API_KEY { return Status::Forbidden }
 	} else {
--- Server/src/main.rs
+++ Server/src/main.rs
@@ -4,21 +4,47 @@ use rocket::http::Status;
 mod api;
 mod conf;
 
-#[get("/world")]
-fn world() -> &'static str {
-    "Hello, world!"
-}
-
 #[get("/")]
 fn base() -> (Status, &'static str) {
     (Status::NotAcceptable, "Please don't do this to me")
 }
 
 #[launch]
-fn rocket() -> _ {
+pub fn rocket() -> _ {
 	let mut custom_conf = rocket::config::Config::default();
 	custom_conf.port = conf::PORT;
 	rocket::custom(custom_conf)
 		.mount("/api", routes![api::submit])
-		.mount("/hello", routes![world])
+}
+
+#[cfg(test)]
+mod test {
+	use super::rocket;
+    use rocket::local::blocking::Client;
+    use rocket::http::{Status, Cookie};
+	use super::conf::API_KEY;
+	
+	#[cfg(test)]
+	mod api {
+		use super::*;
+		
+		#[test]
+		fn submit() {
+			let client = Client::tracked(rocket()).expect("valid rocket instance");
+			let mut resp = client.post(uri!("/api/submit"))
+				.cookie(Cookie::new("key", API_KEY))
+				.body("{\"a\":\"b\"}")
+				.dispatch();
+			assert_eq!(resp.status(), Status::Ok);
+		}
+		
+		#[test]
+		fn submit_bad_body() {
+			let client = Client::tracked(rocket()).expect("valid rocket instance");
+			let mut resp = client.post(uri!("/api/submit"))
+				.cookie(Cookie::new("key", API_KEY))
+				.dispatch();
+			assert_eq!(resp.status(), Status::BadRequest);
+		}
+	}
 }
