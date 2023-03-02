55d3408474553e3c9a3f4682fb23f0eba27a0578
Callum Gray <81529257+callum-gg@users.noreply.github.com>
2023-03-02 13:12:42 +0000

    Added basic menu assets

---
+++ Unity/Menu/Assets/MenuButtons.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuButtons : MonoBehaviour
+{
+    public void LoadScene(string scene_name)
+    {
+        SceneManager.LoadScene(scene_name);
+    }
+}
